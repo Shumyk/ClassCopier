5a125cf8c9c0df5a95c06af4c0d67930dec0e695
shumyk <dima.shumyk@gmail.com>
2019-08-15 17:54:25 +0300

    Refactored main class, added documentation.

--- src/com/shumyk/classcopier/actions/CopyClassFiles.java
+++ src/com/shumyk/classcopier/actions/CopyClassFiles.java
@@ -10,11 +10,15 @@ import com.shumyk.classcopier.module.ModuleWorker;
 import com.shumyk.classcopier.notificator.NotificationWorker;
 import com.shumyk.classcopier.paths.PathBusiness;
 
-import java.util.Collection;
 import java.util.List;
 
 public class CopyClassFiles extends AnAction {
 
+    /**
+     * Action which performs search for changed files in VSC, determines if it's Java file and if yes searches those
+     * compiled version in compiler output location. Found compiled classes is copied to location of changed files.
+     * @param event - object with info about user's environment
+     */
     @Override public void actionPerformed(AnActionEvent event) {
         Project project = event.getProject();
         if (project == null) return;
@@ -23,23 +27,22 @@ public class CopyClassFiles extends AnAction {
         String compilerOut = ModuleWorker.getCompilerOutput(project);
         List<String> sourceRoots = ModuleWorker.getSourceRoots(project);
 
-        LocalChangeList localChangeList = ChangeListManager.getInstance(project).getDefaultChangeList();
-        Collection<Change> changes = localChangeList.getChanges();
-
-        changes.stream()
-                .filter(el -> {
-                    if (el.getVirtualFile() == null) return false;
-                    String filePath = el.getVirtualFile().getPath();
-                    boolean isJavaFile = PathBusiness.endsWithJava(filePath);
-                    if (el.getFileStatus() == FileStatus.ADDED && isJavaFile)
-                        notificationWorker.addNewFile(PathBusiness.extensionToClass(filePath));
-                   return isJavaFile;
-                })
-                .forEach(el -> copyClassFile(el, compilerOut, sourceRoots, notificationWorker));
+        ChangeListManager.getInstance(project).getDefaultChangeList().getChanges()
+                .stream().filter(change -> isJavaFile(change, notificationWorker))
+                .forEach(change -> copyClassFile(change, compilerOut, sourceRoots, notificationWorker));
 
         notificationWorker.doNotify();
     }
 
+    /**
+     * Creates all needed paths and then perform search for class file in output location.
+     * Searches for provided changed file compiled class.
+     * Then performs for every found file copy operation to source location.
+     * @param change - wrapper over changed file
+     * @param compilerOut - location of compiled classes
+     * @param sourceRoots - source roots of project's modules
+     * @param notificationWorker - NotificationWorker is passed here further for collecting error messages.
+     */
     private void copyClassFile(final Change change, final String compilerOut,
                                final List<String> sourceRoots, final NotificationWorker notificationWorker) {
         if (change.getVirtualFile() == null) return;
@@ -53,5 +56,24 @@ public class CopyClassFiles extends AnAction {
 
         FilesWorker.findFiles(compilerOutputUrl, filename, notificationWorker)
                 .forEach(file -> FilesWorker.copyFile(file, javaAbsoluteDirLocation, notificationWorker));
+    }
+
+    /**
+     * Performs check is provided change is for Java file.
+     * If file is Java class and it is new file it adds notification for user to not forget add it to VCS.
+     * @param change - wrapper over changed file
+     * @param notificationWorker - NotificationWorker used in case of situation when file is new and Java format in order to deliver remainder to user.
+     * @return (false|true)
+     */
+    private boolean isJavaFile(Change change, final NotificationWorker notificationWorker) {
+        if (change.getVirtualFile() == null) return false;
+
+        String filePath = change.getVirtualFile().getPath();
+        boolean isJavaFile = PathBusiness.endsWithJava(filePath);
+
+        if (change.getFileStatus() == FileStatus.ADDED && isJavaFile)
+            notificationWorker.addNewFile(PathBusiness.extensionToClass(filePath));
+
+        return isJavaFile;
     }
 }
